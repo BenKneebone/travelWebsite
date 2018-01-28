fa86aa24fd1cd8b3b6021ef2411e540c15c09452
BenKneebone <benkneebone1993@gmail.com>
2018-01-28 20:25:51 +0000

    Black/White Map Styling

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,7 +1,6 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 describe('AppComponent', () => {
-  let jQuery;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,21 @@ declare var jQuery: any;
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements AfterViewInit{
+export class AppComponent implements AfterViewInit {
   title = 'app';
 
   ngAfterViewInit(): void {
-    jQuery('#world-map').vectorMap();
+    jQuery('#world-map').vectorMap({
+      backgroundColor: '',
+      regionStyle: {
+        initial: {
+          fill: 'white',
+          'fill-opacity': 1,
+          stroke: 'black',
+          'stroke-width': 1,
+          'stroke-opacity': 1
+        }
+      }
+    });
   }
 }
