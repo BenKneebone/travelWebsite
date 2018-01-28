769b8507a0040948c004192c81ae08ef3f348ba2
BenKneebone <benkneebone1993@gmail.com>
2018-01-28 18:36:52 +0000

    Initial push with working JVectorMap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component} from '@angular/core';
+
+declare var jQuery: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
   title = 'app';
+
+  ngAfterViewInit(): void {
+    jQuery('#world-map').vectorMap();
+  }
 }
